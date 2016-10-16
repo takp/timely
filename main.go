06be99be4968e4cc915ee269b84277f3fc1f7a48
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2016-10-16 10:37:29 +0700

    Use urfave/cli package for support subcommnad

--- main.go
+++ main.go
@@ -3,24 +3,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"github.com/urfave/cli"
 )
 
 func main() {
 
-	if len(os.Args) < 2 {
-		fmt.Println("Run all...")
-		getAll()
-		return
-	}
-	subcommand := os.Args[1]
-	fmt.Println("Subcommand: ", subcommand)
+	app := cli.NewApp()
+	app.Name = "timely"
+	app.Usage = "get an timely info"
 
-	switch subcommand {
-	case "q": qiita()
-	case "qiita": qiita()
-	case "g": github()
-	case "github": github()
+	app.Commands = []cli.Command{
+		{
+			Name:    "qiita",
+			Aliases: []string{"q"},
+			Usage:   "Get timely info from qiita",
+			Action:  func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				//getAll()
+				return nil
+			},
+		},
+		{
+			Name:    "github",
+			Aliases: []string{"g"},
+			Usage:   "Get timely info from github",
+			Action:  func(c *cli.Context) error {
+				fmt.Println("completed task: ", c.Args().First())
+				return nil
+			},
+		},
 	}
+
+	app.Run(os.Args)
 }
 
 func getAll() {
